591ca5320474a6c05f050ed306de792c9ee88c68
hanchumon <leehyj1112@gmail.com>
2023-08-01 03:18:37 +0900

    map 관련 페이지 마크업 및 css 완료 map부분 js구현완료
    map 관련 에러 수정
    createMapAllReview 함수 내부수정
    
    issue: #28 완료

--- client/js/map/reviewMap.js
+++ client/js/map/reviewMap.js
@@ -1,5 +1,4 @@
 import { createReviewMap } from "../../lib/index.js";
 
-  createReviewMap('#reviewMap',37.558048589925534,126.91122939941944)
+createReviewMap('#reviewMap',37.558048589925534,126.91122939941944)
 
-// createReviewMap(37.558048589925534,126.91122939941944)
